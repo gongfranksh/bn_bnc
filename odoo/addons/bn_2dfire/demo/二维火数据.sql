ba8a2d888ffcddfa5e428919b3886043b33f8572
weiliang <weiliang@buynow.com.cn>
2020-05-12 10:28:40 +0800

    modify 2dire order detail

--- odoo/addons/bn_2dfire/demo/二维火数据.sql
+++ odoo/addons/bn_2dfire/demo/二维火数据.sql
@@ -45,3 +45,9 @@ select "orderId"  from  bn_2dfire_order_ordervo where "innerCode" like '20200511
 and "orderId"  in ( select ordersn from bn_2dfire_order where store_code='131360'))
 
 
+      select sum(fee)  FROM bn_2dfire_order_orderlist
+                        where "orderId" in (
+                        select "orderId"  from  bn_2dfire_order_ordervo where "innerCode" like '20200511%'
+                        and "orderId"  in ( select ordersn from bn_2dfire_order where store_code='131360'))
+
+
